89b5b9fed1d21e94442675be171325b2f7f3756d
Alexandre0911 <goncalo.alexandre.0911@gmail.com>
2024-09-03 08:04:41 +0100

    Added Initial Items, Blocks and Supported Languages:
    # Items:
    - Bismuth
    - Raw Bismuth
    
    # Blocks:
    - Bismuth Ore
    - Bismuth Block
    
    # Languages:
    - EN_US - US English
    - PT_BR - Português Brasil
    - PT_PT - Português Portugal

--- src/main/java/net/alex/tutorialmod/TutorialMod.java
+++ src/main/java/net/alex/tutorialmod/TutorialMod.java
@@ -1,5 +1,9 @@
 package net.alex.tutorialmod;
 
+import net.alex.tutorialmod.block.ModBlocks;
+import net.alex.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +38,9 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +53,16 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+        }
+        if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_ORE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
